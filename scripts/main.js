7e56dca41513f422bfb6ff260d73d04a810adf5d
angelr1076 <angelr1076@gmail.com>
2022-08-26 17:44:50 -0400

    Added asset, style and script tags

--- scripts/main.js
+++ scripts/main.js
@@ -2,20 +2,22 @@ let gameButton = document.querySelector('#start-game');
 let rockButton = document.querySelector('#rock');
 let paperButton = document.querySelector('#paper');
 let scissorButton = document.querySelector('#scissors');
-let display = document.querySelector('#game-display');
-let playerScore = document.querySelector('.player-score');
-let computerScore = document.querySelector('.computer-score');
-let roundDecision = document.querySelector('.round-decision');
-let gameDecision = document.querySelector('.game-decision');
+let gameDisplay = document.querySelector('#game-display');
+let playerScore = document.querySelector('#player-score');
+let computerScore = document.querySelector('#computer-score');
+let roundDecision = document.querySelector('#round-decision');
+let roundDisplay = document.querySelector('#round-display');
+let gameDecision = document.querySelector('#game-decision');
 let computerPoints = 0;
 let playerPoints = 0;
+let roundCount = 0;
 let gameActive = true;
 let playerChoice;
 
 // Begin with a function called getComputerChoice
 function getComputerChoice() {
   // Create computer choices
-  let computerChoices = ['rock', 'paper', 'scissors'];
+  let computerChoices = ['Rock', 'Paper', 'Scissors'];
   // Randomly generate a choice
   let randomChoice = Math.floor(Math.random() * computerChoices.length);
   // Return the choice, uppercase
@@ -27,65 +29,69 @@ function playRound(playerSelection, computerSelection) {
   // Define win/lose parameters
   if (playerSelection == computerSelection) {
     roundDecision.textContent = 'Tie game.';
-    renderDisplay();
+    // renderDisplay();
     return roundDecision.textContent;
   } else if (
     // The function should - and then return a string that declares the winner of the round
     // Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-    (computerSelection == 'rock' && playerSelection == 'scissors') ||
-    (computerSelection == 'paper' && playerSelection == 'rock') ||
-    (computerSelection == 'scissors' && playerSelection == 'paper')
+    (computerSelection == 'Rock' && playerSelection == 'Scissors') ||
+    (computerSelection == 'Paper' && playerSelection == 'Rock') ||
+    (computerSelection == 'Scissors' && playerSelection == 'Paper')
   ) {
-    roundDecision.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
-    renderDisplay();
+    roundDecision.textContent = `You lose!`;
     return roundDecision.textContent;
   } else if (
     // The function should - and then return a string that declares the winner of the round
     // Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-    (playerSelection == 'rock' && computerSelection == 'scissors') ||
-    (playerSelection == 'paper' && computerSelection == 'rock') ||
-    (playerSelection == 'scissors' && computerSelection == 'paper')
+    (playerSelection == 'Rock' && computerSelection == 'Scissors') ||
+    (playerSelection == 'Paper' && computerSelection == 'Rock') ||
+    (playerSelection == 'Scissors' && computerSelection == 'Paper')
   ) {
-    roundDecision.textContent = `You win! ${playerSelection} beats ${computerSelection} ${playerSelection}.`;
-    renderDisplay();
+    roundDecision.textContent = `You win!`;
     return roundDecision.textContent;
   }
 }
 
 // Write a NEW function called game().
+// Called by event listeners
 function initGame() {
   let computerSelection = getComputerChoice();
   let play = playRound(playerChoice, computerSelection);
   let winner;
-  console.log(play);
+  roundCount++;
+  playerScore.innerHTML = `${playerPoints} <br> Chose: ${playerChoice.toUpperCase()}`;
+  computerScore.innerHTML = `${computerPoints} <br> Chose: ${computerSelection.toUpperCase()}`;
 
-  if (play.includes('You lose!')) {
+  if (play.includes('You win!')) {
+    playerPoints++;
+    roundDisplay.textContent = roundCount;
+    roundDecision.textContent = 'Player wins this round';
+    winner = 'Player';
+    playerPoints === 5 ? callGame(playerPoints, winner) : (gameActive = true);
+  } else if (play.includes('You lose!')) {
     computerPoints++;
-    computerScore.innerHTML = `Computer score: ${computerPoints}`;
+    roundDisplay.textContent = roundCount;
     roundDecision.textContent = 'Computer wins this round';
     winner = 'Computer';
     computerPoints === 5
       ? callGame(computerPoints, winner)
       : (gameActive = true);
-  } else if (play.includes('You win!')) {
-    playerPoints++;
-    console.log(playerPoints);
-    playerScore.innerHTML = `Player score: ${playerPoints}`;
-    roundDecision.textContent = 'Player wins this round';
-    winner = 'Player';
-    playerPoints === 5 ? callGame(playerPoints, winner) : (gameActive = true);
   } else {
-    roundDecision.textContent = `It's a tie! The player and the computer both chose ${computerSelection}.`;
+    roundDisplay.textContent = roundCount;
+    // playerScore.innerHTML = `${playerPoints} <br> Chose: ${playerChoice.toUpperCase()}`;
+    // computerScore.innerHTML = `${computerPoints} <br> Chose: ${computerSelection.toUpperCase()}`;
+    roundDecision.textContent = `It's a tie! The player and the computer both chose ${computerSelection.toUpperCase()}.`;
   }
 }
 
 // ----------------------- Game decision -----------------------
 
 function callGame(points, whoWon) {
-  gameDecision.textContent = `${whoWon} wins the game with ${points}`;
-  setTimeout(() => {
-    gameOver();
-  }, 3000);
+  gameDecision.textContent = `${whoWon} wins the game with ${points} points`;
+  roundDecision.classList.remove('round-decision');
+  roundDecision.classList.add('end-game');
+  roundDecision.textContent = 'Game Over';
+  gameOver();
 }
 
 // ----------------------- End/reset -----------------------
@@ -94,6 +100,7 @@ function gameOver() {
   computerPoints = 0;
   playerPoints = 0;
   gameActive = false;
+  setChoicesInactive();
 }
 
 function resetGame() {
@@ -102,10 +109,13 @@ function resetGame() {
 
 // ----------------------- Display -----------------------
 
-function renderDisplay() {
-  display.appendChild(playerScore);
-  display.appendChild(computerScore);
-  display.appendChild(roundDecision);
+function setChoicesInactive() {
+  rockButton.classList.add('inactive');
+  paperButton.classList.add('inactive');
+  scissorButton.classList.add('inactive');
+  rockButton.disabled = true;
+  paperButton.disabled = true;
+  scissorButton.disabled = true;
 }
 
 // ----------------------- Game buttons -----------------------
@@ -118,16 +128,16 @@ gameButton.addEventListener('click', () => {
 // ----------------------- Player choices -----------------------
 
 rockButton.addEventListener('click', e => {
-  playerChoice = e.target.value.toLowerCase();
+  playerChoice = e.target.value;
   initGame();
 });
 
 paperButton.addEventListener('click', e => {
-  playerChoice = e.target.value.toLowerCase();
+  playerChoice = e.target.value;
   initGame();
 });
 
 scissorButton.addEventListener('click', e => {
-  playerChoice = e.target.value.toLowerCase();
+  playerChoice = e.target.value;
   initGame();
 });
